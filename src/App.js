bf68af3ec2d398470d00807519b0521ff16ff97f
David Lin <haiflin@iu.edu>
2019-05-25 14:14:50 -0400

    Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+// import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -60,7 +61,7 @@ class App extends Component {
     // };
 
     let persons = null;
-    let btnClass = ';'
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -87,15 +88,15 @@ class App extends Component {
     }
     return (
 
-        <div className={classes.App}>
-          <h1>Hi, I'm a React App</h1>
-          <p className={assignedClasses.join(' ')}>This is really working!</p>
-          <button
-            // style={style}
-            className = {btnClass}
-            onClick={this.togglePersonHandler}>Switch Name</button>
-          {persons}
-        </div>
+      <div className={classes.App}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button
+          // style={style}
+          className={btnClass}
+          onClick={this.togglePersonHandler}>Switch Name</button>
+        {persons}
+      </div>
 
 
     );
